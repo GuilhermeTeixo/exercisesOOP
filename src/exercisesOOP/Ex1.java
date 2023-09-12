b0ee1d82ba0c57d5fffb55edabc12cb2abf513f3
Guilherme Teixo <guilhermesantosjp@gmail.com>
2023-09-12 16:21:44 -0300

    first exercise ok, two more left

--- src/exercisesOOP/Ex1.java
+++ src/exercisesOOP/Ex1.java
@@ -14,8 +14,14 @@ public class Ex1 {
 		Rectangle rectangle = new Rectangle();
 		System.out.println("Enter rectangle width and height:");
 		rectangle.width=sc.nextDouble();
+		rectangle.height=sc.nextDouble();
 		
+		double area = rectangle.area();
+		double perimeter = rectangle.perimeter();
+		double diagonal =  rectangle.diagonal();
 		
+		System.out.printf("AREA = %.2f%nPERIMETER = %.2f%nDIAGONAL = %.2f%n", area, perimeter, diagonal);
+				
 		sc.close();
 	}
 
